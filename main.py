828f229ea8f2e63265537f5955aff7ca89f953c5
tmsblnt <balint200440@gmail.com>
2025-09-02 08:03:16 +0200

    Első fázis

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ uzenet = "helloworld"
 kulcs = "abcdefgijkl"
 
 rejtjel = ""
+
+for i in range(len(uzenet)):
+    uzenet_karakter = uzenet[i]
+    kulcs_karakter = kulcs[i]
+    hely_uzenet = abc.index(uzenet_karakter)
+    hely_kulcs = abc.index(kulcs_karakter)
+    kod = (hely_uzenet + hely_kulcs) % 27
+    rejtjel = abc[kod]
+print(rejtjel)
