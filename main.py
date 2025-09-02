f7d1543d5d35a04482ec2793fbad270fb0339d4a
tmsblnt <balint200440@gmail.com>
2025-09-02 08:07:51 +0200

    1. teljes rész
    Rejtjelezés titkosítása, majd visszafejtése

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ abc = "abcdefghijklmnopqrstuvwxyz "
 uzenet = "helloworld"
 kulcs = "abcdefgijkl"
 
+
+# 1) Rejtjelezés
 rejtjel = ""
 
 for i in range(len(uzenet)):
@@ -11,5 +13,19 @@ for i in range(len(uzenet)):
     hely_uzenet = abc.index(uzenet_karakter)
     hely_kulcs = abc.index(kulcs_karakter)
     kod = (hely_uzenet + hely_kulcs) % 27
-    rejtjel = abc[kod]
+    rejtjel = rejtjel + abc[kod]
 print(rejtjel)
+
+# 2) Visszafejtés
+
+fejtett = ""
+
+for i in range(len(rejtjel)):
+    rejtjel_karakter = rejtjel[i]
+    kulcs_karakter = kulcs[i]
+    hely_rejtjel = abc.index(rejtjel_karakter)
+    hely_kulcs = abc.index(kulcs_karakter)
+    kod = (hely_uzenet - hely_kulcs) % 27
+    fejtett = fejtett + abc[kod]
+
+print(fejtett)
