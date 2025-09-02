f2fb5178a973f25d67728d94c4bc1f0bd03f5b1a
tmsblnt <balint200440@gmail.com>
2025-09-02 08:19:45 +0200

    Függvényesítés

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 abc = "abcdefghijklmnopqrstuvwxyz "
 
-uzenet = "helloworld"
-kulcs = "abcdefgijkl"
-
+# 1. feladat
 
 # 1) Rejtjelezés
-rejtjel = ""
-
-for i in range(len(uzenet)):
-    uzenet_karakter = uzenet[i]
-    kulcs_karakter = kulcs[i]
-    hely_uzenet = abc.index(uzenet_karakter)
-    hely_kulcs = abc.index(kulcs_karakter)
-    kod = (hely_uzenet + hely_kulcs) % 27
-    rejtjel = rejtjel + abc[kod]
-print(rejtjel)
+def titkosit(uzenet,kulcs):
+    rejtjel = ""
+    for i in range(len(uzenet)):
+        uzenet_karakter = uzenet[i]
+        kulcs_karakter = kulcs[i]
+        hely_uzenet = abc.index(uzenet_karakter)
+        hely_kulcs = abc.index(kulcs_karakter)
+        kod = (hely_uzenet + hely_kulcs) % 27
+        rejtjel = rejtjel + abc[kod]
+    return rejtjel
 
 # 2) Visszafejtés
 
-fejtett = ""
+def fejtes(rejtjel, kulcs):
+    fejtett = ""
+    for i in range(len(rejtjel)):
+        rejtjel_karakter = rejtjel[i]
+        kulcs_karakter = kulcs[i]
+        hely_rejtjel = abc.index(rejtjel_karakter)
+        hely_kulcs = abc.index(kulcs_karakter)
+        kod = (hely_rejtjel - hely_kulcs) % 27
+        fejtett = fejtett + abc[kod]
+    return fejtett
 
-for i in range(len(rejtjel)):
-    rejtjel_karakter = rejtjel[i]
-    kulcs_karakter = kulcs[i]
-    hely_rejtjel = abc.index(rejtjel_karakter)
-    hely_kulcs = abc.index(kulcs_karakter)
-    kod = (hely_uzenet - hely_kulcs) % 27
-    fejtett = fejtett + abc[kod]
-
-print(fejtett)
+print(titkosit("helloworld","abcdefgijkl") + "\n" + fejtes("helloworld","abcdefgijkl"))
